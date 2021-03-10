0c5c1aa20e975d6e4248fa4ad2273d02f6c0dad0
isso-719 <i@kazukiisogai.net>
2021-03-10 17:43:16 +0900

    fix slide

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // slikを実行する https://kenwheeler.github.io/slick/ を参考
 $('.mypattern').slick({
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 10000,
     speed: 300,
     arrows: true,
     centerMode: true,
     centerPadding: '10%',
     slidesToShow: 3,
     infinite: true,
+    variableWidth: true,
 });
