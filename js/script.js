a7b61206d70fdc0e08789c9a6811e4f958b680a0
isso-719 <i@kazukiisogai.net>
2021-03-11 09:10:26 +0900

    add modal

--- js/script.js
+++ js/script.js
@@ -10,3 +10,39 @@ $('.mypattern').slick({
     infinite: true,
     variableWidth: true,
 });
+
+$(function () {
+    $('#openModalIsso').click(function(){
+        $('#modalAreaIsso').fadeIn();
+    });
+    $('#closeModalIsso , #modalBgIsso').click(function(){
+        $('#modalAreaIsso').fadeOut();
+    });
+});
+
+$(function () {
+    $('#openModalNagadon').click(function(){
+        $('#modalAreaNagadon').fadeIn();
+    });
+    $('#closeModalNagadon , #modalBgNagadon').click(function(){
+        $('#modalAreaNagadon').fadeOut();
+    });
+});
+
+$(function () {
+    $('#openModalNinomae').click(function(){
+        $('#modalAreaNinomae').fadeIn();
+    });
+    $('#closeModalNinomae , #modalBgNinomae').click(function(){
+        $('#modalAreaNinomae').fadeOut();
+    });
+});
+
+$(function () {
+    $('#openModalWaigoma').click(function(){
+        $('#modalAreaWaigoma').fadeIn();
+    });
+    $('#closeModalWaigoma , #modalBgWaigoma').click(function(){
+        $('#modalAreaWaigoma').fadeOut();
+    });
+});
