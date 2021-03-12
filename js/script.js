b9676db806e9dc126d83b220add93920dc64f936
Kazuki Isogai <kazu0719_i@ybb.ne.jp>
2021-03-13 00:19:12 +0900

    fix page link animation, hover shadow, slick focus outline

--- js/script.js
+++ js/script.js
@@ -92,3 +92,15 @@ $(window).on('scroll', function () {
         $('#c-scrolldown').fadeIn();
     }
 });
+
+// ページ内リンク
+$(function(){
+    $('a[href^="#"]').click(function(){
+        let speed = 500;
+        let href= $(this).attr("href");
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        let position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
